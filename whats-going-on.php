60ca2458d1aa16c91d8c96ec1095f2a4b228332f
Jaime Niñoles-Manzanera Jimeno <info@jnjsite.com>
2020-07-17 18:49:03 +0200

    Cronjob batches of 100 items for update countries.

--- whats-going-on.php
+++ whats-going-on.php
@@ -19,6 +19,8 @@ include_once WGOJNJ_PATH.'inc/actions.php';
 include_once WGOJNJ_PATH.'inc/filters.php';
 include_once WGOJNJ_PATH.'lib/geoip2.phar';
 
+use GeoIp2\Database\Reader;
+
 function wpdocs_selectively_enqueue_admin_script($hook)
 {
     wp_enqueue_style('wgojnj_custom_style', plugin_dir_url(__FILE__).'lib/wgojnj.css', false, '1.0.1');
@@ -100,12 +102,41 @@ function wgojnj_add_cron_intervals($schedules)
 add_filter('cron_schedules', 'wgojnj_add_cron_intervals');
 function wgojnj_cron_fill_country_columns()
 {
-    /**
-     * Task for seeking countries for IPs, 100 items per table and time..
-     */
-    
+    echo 'Filling countries..'.PHP_EOL;
+
+    global $wpdb;
+    $reader = new Reader(WGOJNJ_PATH.'lib/GeoLite2-City.mmdb');
+    $im_behind_proxy = get_option('wgojnj_im_behind_proxy');
+
+    $tableNames = ['whats_going_on', 'whats_going_on_block', 'whats_going_on_404s'];
+
+    foreach ($tableNames as $tableName) {
+        $sql = 'SELECT * FROM '.$wpdb->prefix.$tableName.' WHERE country_code IS NULL LIMIT 100;';
+        $results = $wpdb->get_results($sql);
+        foreach ($results as $result) {
+            echo $result->remote_ip.'.. ';
+            $ips = explode('-', $result->remote_ip);
+
+            if ($im_behind_proxy) {
+                $ip = $ips[0];
+            } else {
+                $ip = $ips[2];
+            }
+
+            try {
+                $record = $reader->city($ip);
+
+                // Update records..
+                $sql = 'UPDATE '.$wpdb->prefix.$tableName.' SET country_code = \''.$record->country->isoCode.'\' WHERE remote_ip = \''.$result->remote_ip.'\';';
+                $wpdb->get_results($sql);
+                echo 'Saving '.$result->remote_ip.' '.$record->country->isoCode;
+            } catch (\Throwable $th) {
+            }
+            echo PHP_EOL;
+        }
+    }
 }
 add_action('wgojnj_cron_fill_country_columns_hook', 'wgojnj_cron_fill_country_columns');
 if (!wp_next_scheduled('wgojnj_cron_fill_country_columns_hook')) {
-    wp_schedule_event(time(), 'five-minutes', 'wgojnj_cron_fill_country_columns_hook');
+    wp_schedule_event(time(), 'minutely', 'wgojnj_cron_fill_country_columns_hook');
 }
