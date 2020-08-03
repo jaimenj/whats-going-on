39b2354b753cd56413e7234d58882bf7f3fa2d45
Jaime Niñoles-Manzanera Jimeno <info@jnjsite.com>
2020-08-03 22:18:53 +0200

    Refactor uninstall.

--- whats-going-on.php
+++ whats-going-on.php
@@ -118,9 +118,23 @@ class WhatsGoingOn
         $sql = 'DROP TABLE '.$wpdb->prefix.'whats_going_on_404s;';
         $wpdb->get_results($sql);
 
-        if (file_exists(ABSPATH.'.user.ini')) {
-            unlink(ABSPATH.'.user.ini');
+        WhatsGoingOnBackendController::get_instance()->_uninstall_waf();
         }
+
+    public function uninstall()
+    {
+        delete_option('wgojnj_limit_requests_per_minute');
+        delete_option('wgojnj_limit_requests_per_hour');
+        delete_option('wgojnj_items_per_page');
+        delete_option('wgojnj_days_to_store');
+        delete_option('wgojnj_im_behind_proxy');
+        delete_option('wgojnj_notification_email');
+        delete_option('wgojnj_notify_requests_more_than_sd');
+        delete_option('wgojnj_notify_requests_more_than_2sd');
+        delete_option('wgojnj_notify_requests_more_than_3sd');
+        delete_option('wgojnj_save_payloads');
+
+        WhatsGoingOnBackendController::get_instance()->_uninstall_waf();
     }
 
     /**
